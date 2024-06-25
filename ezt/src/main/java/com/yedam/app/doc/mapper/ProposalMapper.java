b84f6faad5939a34c60e08fb5af32f050f7524c6
admin <admin@YD05-04>
2024-06-25 18:19:49 +0900

    0625 yj

--- ezt/src/main/java/com/yedam/app/doc/mapper/ProposalMapper.java
+++ ezt/src/main/java/com/yedam/app/doc/mapper/ProposalMapper.java
@@ -7,8 +7,12 @@ import org.apache.ibatis.annotations.Param;
 import com.yedam.app.doc.service.ProposalDetailVO;
 import com.yedam.app.doc.service.ProposalVO;
 import com.yedam.app.req.service.RequestVO;
+import com.yedam.app.usr.service.UserVO;
 
 public interface ProposalMapper {
+	
+	// 유저정보조회
+	public UserVO selectUserInfo(int userNo);
 	
 	// 견적서 의뢰정보조회
 	public RequestVO selectReqInfo(RequestVO requestVO);
@@ -17,12 +21,21 @@ public interface ProposalMapper {
 	public ProposalVO selectPpsInfo(ProposalVO proposalVO);
 	
 	// 견적서 상세조회
-	public List<ProposalDetailVO> selectPpszDetailList(@Param("proposalNo")int proposalNo);
+	public List<ProposalDetailVO> selectPpsDetailList(@Param("proposalNo")int proposalNo);
 	
 	// 견적서 등록
 	public int insertPpsInfo(ProposalVO proposalVO);
 
 	// 견적서 상세 등록
 	public int insertPpsDetailInfo(ProposalDetailVO proposalDetailVO);
+	
+	// 견적서 수정
+	public int updatePpsInfo(ProposalVO proposalVO);
+	
+	// 견적서 삭제
+	public int deletePpsInfo(@Param("proposalNo")int proposalNo);
+	
+	// 견적서 상세 삭제
+	public int deletePpsDetailInfo(@Param("proposalNo")int proposalNo);
 }
 
--- ezt/src/main/java/com/yedam/app/doc/service/ProposalService.java
+++ ezt/src/main/java/com/yedam/app/doc/service/ProposalService.java
@@ -1,8 +1,12 @@
 package com.yedam.app.doc.service;
 
 import com.yedam.app.req.service.RequestVO;
+import com.yedam.app.usr.service.UserVO;
 
 public interface ProposalService {
+	
+	// 유저정보조회
+	public UserVO userInfo(int userNo);
 	
 	// 견적서 의뢰정보조회
 	public RequestVO reqInfo(RequestVO requestVO);
@@ -12,6 +16,12 @@ public interface ProposalService {
 	
 	// 견적서 등록
 	public int ppsInsert(ProposalVO proposalVO);
+	
+	// 견적서 수정
+	public int ppsUpdate(ProposalVO proposalVO);
+	
+	// 견적서 삭제
+	public int ppsDelete(int proposalNo);
 
 }
 
