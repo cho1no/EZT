99c8f8cd34bb333c40891c5675b737c5666ad0c6
admin <admin@YD05-19>
2024-06-28 18:02:20 +0900

    240628FIX

--- ezt/src/main/java/com/yedam/app/sgi/config/SpringSecurityConfig.java
+++ ezt/src/main/java/com/yedam/app/sgi/config/SpringSecurityConfig.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.yedam.app.sgi.handler.LoginFailHandler;
 
 @Configuration
 @EnableWebSecurity
