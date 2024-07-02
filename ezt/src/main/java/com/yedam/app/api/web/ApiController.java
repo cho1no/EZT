e1c83895eb902935d5969cb60553b73dee0fe6dc
cho1no <rkawk9735@gmail.com>
2024-07-02 17:37:41 +0900

    240702_CWH_NH_developer_Api

--- ezt/src/main/java/com/yedam/app/api/web/ApiController.java
+++ ezt/src/main/java/com/yedam/app/api/web/ApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yedam.app.api.service.DatahubService;
+import com.yedam.app.api.service.NhDevService;
 import com.yedam.app.api.service.PublicDataService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,27 +21,57 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api")
 @Slf4j
 public class ApiController {
-	@Autowired
-	DatahubService datahubSvc;
-	@Autowired
-	PublicDataService publicDataSvc;
+	@Autowired DatahubService datahubSvc;
+	@Autowired PublicDataService publicDataSvc;
+	@Autowired NhDevService nhDevSvc;
 
-	// datahub 신분증 확인 api
+	/** 
+	 * datahub 신분증 확인 api
+	 * @param map
+	 * @return JSON
+	 */
 	@PostMapping("/idCardCheck")
 	public Mono<Map> idCard(@RequestBody Map<String, String> map) {
 		return datahubSvc.idCardCheck(map);
 	}
-	
+	/** 
+	 * datahub 운전면허증 확인 api
+	 * @param map
+	 * @return JSON
+	 */
 	@PostMapping("/driveLicenseCheck")
 	public Mono<Map> driveLicense(@RequestBody Map<String, String> map) {
 		log.info(map.toString());
 		return datahubSvc.driveLicenseCheck(map);
 	}
 
-	// 국가 기술 자격증 확인 api
+	/**
+	 *  국가 기술 자격증 확인 api
+	 * @param licenseId - String
+	 * @return
+	 */
 	@GetMapping("/techCheck/{licenseId}")
 	public Mono<Map> techLicense(@PathVariable String licenseId){
 		return publicDataSvc.TechLicenseCheck(licenseId);
 	}
-
+	
+	/**
+	 *  NH 가상계좌 발급
+	 * @return
+	 */
+	@GetMapping("/getVirtualAc")
+	public Mono<Map> getVirtualAc(){
+		return nhDevSvc.getVirtualAc();
+	}
+	/**
+	 *  NH 가상계좌 입금 조회
+	 * @param vran - 계좌번호, 미입력시 전체 조회
+	 * @return
+	 */
+	@GetMapping("/getVirtualAcRcvList")
+	public Mono<Map> getVirtualAcRcvList(String vran){
+		if (vran == null) vran="";
+		return nhDevSvc.getVirtualAcRecieveList(vran);
+	}
+	
 }
