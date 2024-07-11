96c5bcd7aa609714d4e0ceead4e8d89395d8f5e3
admin <admin@YD05-19>
2024-07-11 16:08:22 +0900

    KSH0711
    로그인컨트롤러 로그삭제

--- ezt/src/main/java/com/yedam/app/sgi/web/SgiController.java
+++ ezt/src/main/java/com/yedam/app/sgi/web/SgiController.java
@@ -32,8 +32,6 @@ public class SgiController {
 	public String login(@RequestParam(value = "error", required = false) String error,
             @RequestParam(value = "exception", required = false) String exception, Model model) {
         model.addAttribute("error", error);
-        log.info(exception);
-        log.info(error);
         model.addAttribute("exception", exception);
 		return "sgi/login";
 	}
